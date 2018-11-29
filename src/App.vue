ee4fa93050ffcfc91401143645dab37ccf7e4d66
“gliqin <15101614995@63.com>
2018-11-29 16:52:44 +0800

    项目搭建

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="btn"></div>
+  </div>
+  
+</template>
+<script>
+export default {}
+</script>
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+  @import './common/stylus/mixins.styl'
+  .btn
+    width px2rem(200)
+    height px2rem(300)
+    background-color red
+</style>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import store from './store'
+import VueLazyload from 'vue-lazyload'
+import loading from './common/images/loding2.gif'
+
+// 图片懒加载
+Vue.use(VueLazyload, {
+  loading
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
